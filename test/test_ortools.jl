27cb85e0797c0f770be66e1804968e97b9e3e850
Leo Penneman <leo.pennemandb@gmail.com>
2026-07-24 14:23:42 +0200

    Added test_vrppd execution to ortools test suite

--- test/test_ortools.jl
+++ test/test_ortools.jl
@@ -6,3 +6,10 @@ using Test
 @testset "MathOptVRP.test_vrp (ORTools/CP-SAT)" begin
     MathOptVRP.Tests.test_vrp(MathOptVRP.ortools_optimizer)
 end
+
+@testset "MathOptVRP.test_vrppd (ORTools/CP-SAT)" begin
+    MathOptVRP.Tests.test_vrppd(
+        MathOptVRP.ortools_optimizer;
+        read_routes = _read_routes,
+    )
+end
