59e510a9c3be84ed62ce4e740a685a103fdec29e
Benoît Legat <benoit.legat@gmail.com>
2026-08-04 16:07:07 +0200

    Remove type piracy (#15)

--- src/operators.jl
+++ src/operators.jl
@@ -19,24 +19,3 @@ with at least one JuMP-tainted argument it constructs a
 `GenericNonlinearExpr` with head `:sum_distances`.
 """
 const op_sum_distances = JuMP.NonlinearOperator(sum_distances, :sum_distances)
-
-# Below are type piracy that can be removed once
-# https://github.com/jump-dev/JuMP.jl/pull/3451 is merged. They let
-# JuMP accept arrays as leaves of `GenericNonlinearExpr` (needed for
-# `op_sum_distances(dist_matrix, nodes)`), broadcast `moi_function` over
-# arrays, and pick a variable-ref type for arrays of JuMP scalars (so
-# `NonlinearOperator` dispatches to expression-building rather than to
-# calling the stub scalar function).
-
-JuMP._is_real(::Array{<:Real}) = true
-
-JuMP._is_real(::Array{<:JuMP.AbstractJuMPScalar}) = true
-
-JuMP.moi_function(x::Array) = JuMP.moi_function.(x)
-
-function JuMP.variable_ref_type(
-    ::Type{JuMP.GenericNonlinearExpr},
-    ::AbstractArray{T},
-) where {T<:JuMP.AbstractJuMPScalar}
-    return JuMP.variable_ref_type(T)
-end
